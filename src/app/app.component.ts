d08081fb9bc4b2721fcb0dc545644686a443023c
Alexis Tercero <alexistercero55@gmail.com>
2025-04-10 11:32:01 -0600

    UPDATE - MODEL + BASIC COMPONENT P27

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './todo-item.model';
+import { TodoList } from './todo-list.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+  private list = new TodoList("Bob", [
+      new TodoItem("Go for run", true),
+      new TodoItem("Get flowers"),
+      new TodoItem("Collect tickets"),
+    ]);
+  get username(): string {
+  return this.list.user;
+  }
+
+  get itemCount(): number {
+  return this.list.items
+  .filter(item => !item.complete).length;
+  }
+
+  get items(): readonly TodoItem[] {
+    return this.list.items.filter(item => this.showComplete || !item.complete);
+  }
+
+  addItem(newItem) {
+    if (newItem != "") {
+      this.list.addItem(newItem);
+    }
+  }
+
+  showComplete: boolean = false;
 }
